ad7dc7c25833cad3b82bcc59ce2477301ec61b0a
MadCowKastor <jonathanpw75@yahoo.co.nz>
2022-01-30 16:22:04 +1300

    Added Spawner

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public GameObject spawnedPrefab;
+    public int spawnsRemaining;
+    public float spawnCooldown;
+    public float spawnRange;
+    private float spawnClock = 0f;
+    
+    public PlayerController playerCon;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCon = FindObjectOfType<PlayerController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnClock < spawnCooldown) { spawnClock += Time.deltaTime; }
+        else
+        {
+            if ((playerCon.transform.position - gameObject.transform.position).magnitude > spawnRange)
+            {
+                spawnClock = 0f;
+                Instantiate<GameObject>(spawnedPrefab, gameObject.transform);
+            }
+        }
+    }
+}
