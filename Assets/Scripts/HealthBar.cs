82ed8a7ea0cb27368de135752ae9f2eda8aaf261
Catherine Illingworth <constelldev@gmail.com>
2022-01-30 15:51:12 +1300

    made a player health bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private static Image HealthBarImage;
+
+    public PlayerController PlayerHeat;
+
+
+
+    /// <summary>
+    /// Sets the health bar value
+    /// </summary>
+    /// <param name="value">should be between 0 to 1</param>
+    /// 
+    void Update()
+    {
+        float value = PlayerHeat.health / 100;
+        SetFill(value);
+    }
+    public void SetFill(float value)
+    {
+        HealthBarImage.fillAmount = value;
+
+    }
+
+    public static float GetBarValue()
+    {
+        return HealthBarImage.fillAmount;
+    }
+
+    /// <summary>
+    /// Sets the health bar color
+    /// </summary>
+    /// <param name="healthColor">Color </param>
+    public static void SetBarColor(Color healthColor)
+    {
+        HealthBarImage.color = healthColor;
+    }
+
+    /// <summary>
+    /// Initialize the variable
+    /// </summary>
+    private void Start()
+    {
+        HealthBarImage = GetComponent<Image>();
+    }
+}
