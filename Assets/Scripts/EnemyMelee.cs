959ae6911dc6b0b4abf735b455b502e9f7c53c6a
Judge Davis <Judgeit2@gmail.com>
2022-01-30 15:46:42 +1300

    Added Enemy Melee collider. Will interact and do damage to the player when it contacts them, and then set itself to be disabled. Enemy melee attacks should enable this (for a time).

---
+++ Assets/Scripts/EnemyMelee.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyMelee : MonoBehaviour
+{
+    public float damage, heatDamage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            PlayerController playerCon = other.GetComponent<PlayerController>();
+            playerCon.health -= damage;
+            playerCon.heat += heatDamage;
+            gameObject.SetActive(false);
+        }
+    }
+}
