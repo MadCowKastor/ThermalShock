70951d83364a99b2da5fc5b936c38827b2f14d6d
Catherine Illingworth <constelldev@gmail.com>
2022-01-30 16:54:05 +1300

    sped up the sword swing and created a game end script

--- Assets/Scripts/GameEnd.cs
+++ Assets/Scripts/GameEnd.cs
@@ -5,15 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class GameEnd : MonoBehaviour
 {
+    public float timeLeft = 5;
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene(0);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver() {
+        SceneManager.LoadScene(0);
     }
 }
