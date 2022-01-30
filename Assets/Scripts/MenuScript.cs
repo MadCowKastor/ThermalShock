0d39e27dcbac8b6328aef553a66790f26ef594c1
MadCowKastor <jonathanpw75@yahoo.co.nz>
2022-01-30 13:05:12 +1300

    Implemented Menu Functionality

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void SceneSwap(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void PressQuit()
+    {
+        Application.Quit();
+    }
+
+}
